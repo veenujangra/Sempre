19daba21d59f3bb272e46c549ef0bea64ebbe2ef
veenujangra <veenuj01@gmail.com>
2024-06-11 17:12:29 +0530

    diabled create on resize

---
+++ .changeset/light-planets-kick.md
@@ -0,0 +1,5 @@
+---
+"@pixeto/sempre": patch
+---
+
+Disabled create on resize
--- src/animations/title/index.js
+++ src/animations/title/index.js
@@ -66,6 +66,6 @@ export default class Title extends Animation {
   animateOut() {}
 
   onResize() {
-    this.create()
+    // this.create()
   }
 }
